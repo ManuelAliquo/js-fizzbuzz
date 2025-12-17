0d73ecafe6534dbf0310dc12784a68848c11da5e
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-17 14:56:02 +0100

    added for loop

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// # Pseudo-codice
+
+// - Diciamo al PC di "contare" da 1 a 100
+// - Se il numero a cui è arrivato è multiplo di 3 dovrà dire "Fizz"
+// - Se il numero a cui è arrivato è multiplo di 5 dovrà dire "Buzz"
+// - Se il numero a cui è arrivato è multiplo di entrambi dovrà dire "FizzBuzz"
+
+for (let number = 1; number <= 100; number++) {
+  console.log(number);
+}
