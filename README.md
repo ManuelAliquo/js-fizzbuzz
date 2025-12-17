12f0e6038a8f2d19d64fdc5ce48ea16b99a4df02
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-17 15:31:14 +0100

    (for ticket)

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Stampare i numeri da 1 a 100, per i multipli di 3 stampa Fizz e di 5 stampa Buzz
 # Pseudo-codice
 
 - Diciamo al PC di "contare" da 1 a 100
-- Se il numero a cui è arrivato è multiplo di 3 e non di 5 dovrà dire "Fizz"
-- Se il numero a cui è arrivato è multiplo di 5 e non di 3 dovrà dire "Buzz"
-- Se il numero a cui è arrivato è multiplo di entrambi dovrà dire "FizzBuzz"
+- Se il numero a cui è arrivato è multiplo di 3 e non di 5 dovrà stampare "Fizz";
+  - Altrimenti Se il numero a cui è arrivato è multiplo di 5 e non di 3 dovrà stampare "Buzz";
+  - Altrimenti Se il numero a cui è arrivato è multiplo di entrambi dovrà stampare "FizzBuzz";
+  - Altrimenti dovrà stampare i numeri "normali";
--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 // # Pseudo-codice
 
 // - Diciamo al PC di "contare" da 1 a 100
-// - Se il numero a cui è arrivato è multiplo di 3 e non di 5 dovrà dire "Fizz"
-// - Se il numero a cui è arrivato è multiplo di 5 e non di 3 dovrà dire "Buzz"
-// - Se il numero a cui è arrivato è multiplo di entrambi dovrà dire "FizzBuzz"
+// - Se il numero a cui è arrivato è multiplo di 3 e non di 5 dovrà stampare "Fizz";
+//   - Altrimenti Se il numero a cui è arrivato è multiplo di 5 e non di 3 dovrà stampare "Buzz";
+//   - Altrimenti Se il numero a cui è arrivato è multiplo di entrambi dovrà stampare "FizzBuzz";
+//   - Altrimenti dovrà stampare i numeri "normali";
 
-for (let number = 1; number <= 30 /*100*/; number++) {
+// con output "interno a if"
+for (let number = 1; number <= 15 /*100*/; number++) {
   if (number % 3 === 0 && number % 5 != 0) {
     console.log("Fizz");
   } else if (number % 5 === 0 && number % 3 != 0) {
@@ -15,4 +17,22 @@ for (let number = 1; number <= 30 /*100*/; number++) {
   } else {
     console.log(number);
   }
+}
+
+console.log("##########################################");
+
+// con output "esterno a if"
+let output;
+
+for (let number = 1; number <= 15 /*100*/; number++) {
+  if (number % 3 === 0 && number % 5 != 0) {
+    output = "Fizz";
+  } else if (number % 5 === 0 && number % 3 != 0) {
+    output = "Buzz";
+  } else if (number % 3 === 0 && number % 5 === 0) {
+    output = "FizzBuzz";
+  } else {
+    output = number;
+  }
+  console.log(output);
 }
