1ef8a9571e0de1a32516e8427ce191a675cf8c71
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-17 17:39:48 +0100

    bonus

--- js/main.js
+++ js/main.js
@@ -6,16 +6,15 @@
 //   - Altrimenti Se il numero a cui è arrivato è multiplo di entrambi dovrà stampare "FizzBuzz";
 //   - Altrimenti dovrà stampare i numeri "normali";
 
-let output;
-
-for (let number = 1; number <= 30 /*100*/; number++) {
-  if (number % 3 === 0 && number % 5 === 0) {
-    output = "FizzBuzz";
-  } else if (number % 5 === 0) {
-    output = "Buzz";
-  } else if (number % 3 === 0) {
-    output = "Fizz";
-  } else {
+for (let number = 1; number <= 100; number++) {
+  let output = "";
+  if (number % 3 === 0) {
+    output += "Fizz";
+  }
+  if (number % 5 === 0) {
+    output += "Buzz";
+  }
+  if (output === "") {
     output = number;
   }
   console.log(output);
