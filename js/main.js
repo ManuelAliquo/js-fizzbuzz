e5b002303ac12c629596cc1cfaa970b9a0b1e758
ManuelAliquo <manuelaliquo2103@gmail.com>
2025-12-17 15:53:15 +0100

    fixed output

--- js/main.js
+++ js/main.js
@@ -6,25 +6,9 @@
 //   - Altrimenti Se il numero a cui è arrivato è multiplo di entrambi dovrà stampare "FizzBuzz";
 //   - Altrimenti dovrà stampare i numeri "normali";
 
-// con output "interno a if"
-for (let number = 1; number <= 15 /*100*/; number++) {
-  if (number % 3 === 0 && number % 5 != 0) {
-    console.log("Fizz");
-  } else if (number % 5 === 0 && number % 3 != 0) {
-    console.log("Buzz");
-  } else if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
-  } else {
-    console.log(number);
-  }
-}
-
-console.log("##########################################");
-
-// con output "esterno a if"
 let output;
 
-for (let number = 1; number <= 15 /*100*/; number++) {
+for (let number = 1; number <= 100; number++) {
   if (number % 3 === 0 && number % 5 != 0) {
     output = "Fizz";
   } else if (number % 5 === 0 && number % 3 != 0) {
